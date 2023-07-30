00e8eb08c8786a86a4438bcc60b86a388bc70b33
sunny <mrunal123wankhade@gmail.com>
2023-07-30 17:06:57 +0530

    Final touch.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from backend import get_data
 
 # Add title,subtitle,widgets
 st.title("Weather Forcast for the 5 Next Days")
-place = st.text_input("place: ")
+place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days ")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
